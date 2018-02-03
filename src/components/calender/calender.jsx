364210ad3da3132c90787f5fa9e7f438789b02dc
Kenny Nguyen <alphakennyn@gmail.com>
2018-02-03 14:21:46 -0500

    v1: Integration complete

---
+++ src/components/calender/calender.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import '../../../node_modules/react-big-calendar/lib/css/react-big-calendar.css';
+import events from './events'
+
+ BigCalendar.momentLocalizer(moment);
+
+
+let Schedule = props => {
+  return (
+    <div>
+    <BigCalendar
+      selectable
+      events={[]}
+      defaultView="week"
+      min={new Date()}
+      scrollToTime={new Date()}
+      defaultDate={new Date()}
+      onSelectEvent={event => alert(event.title)}
+      onSelectSlot={slotInfo =>
+        alert(
+          `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
+            `\nend: ${slotInfo.end.toLocaleString()}` +
+            `\naction: ${slotInfo.action}`
+        )
+      }
+    />
+    </div>
+  );
+    
+}
+
+
+//====================== REDUX CONNECTION =========================
+export default Schedule;
