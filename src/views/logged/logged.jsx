6903b71032083563572b2bedcdfae4043251e869
Jinghao Lin <jinghao.lin@mail.utoronto.ca>
2018-02-03 14:51:06 -0500

    Made sidebar viewable

--- src/views/logged/logged.jsx
+++ src/views/logged/logged.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import SidebarLeft from "../views/sidebar/sidebar";
+import SidebarLeft from "../sidebar/sidebar";
 
 class Main extends React.Component {
   render() {
     return (
       <div>
         <SidebarLeft />
-        You have succesfully logged in! You can't sign out now though. However, it is implemented, you'll have to figure
-        out how to do that :) 
       </div>
     );
   }
--- src/views/sidebar/sidebar.jsx
+++ src/views/sidebar/sidebar.jsx
@@ -1,31 +1,65 @@
-import React, { Component } from "react"
-import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
+import React, { Component } from "react";
+import {
+  Sidebar,
+  Segment,
+  Button,
+  Menu,
+  Icon,
+  Header
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 class SidebarLeft extends Component {
-    state = { visible: false }
+  state = { visible: false };
 
-  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+  toggleVisibility = () => this.setState({ visible: !this.state.visible });
 
   render() {
-    const { visible } = this.state
+    let divStyle = {
+      height: 50 + "em"
+    };
+
+    const { visible } = this.state;
     return (
       <div>
-        <Button onClick={this.toggleVisibility}>Toggle Visibility</Button>
         <Sidebar.Pushable as={Segment}>
-          <Sidebar as={Menu} animation='overlay' width='thin' visible={visible} icon='labeled' vertical inverted>
-            <Menu.Item name='home'>
-              <Icon name='home' />
+          <Sidebar
+            as={Menu}
+            animation="push"
+            width="thin"
+            visible={visible}
+            icon="labeled"
+            vertical
+            inverted
+          >
+            <Menu.Item name="home">
+              <Icon name="home" />
               Home
             </Menu.Item>
-            <Menu.Item name='Groups'>
-              <Icon name='users' />
+            <Menu.Item name="groups">
+              <Icon name="users" />
               Groups
             </Menu.Item>
+            <Menu.Item name="signOut">
+              <Icon name="sign out" />
+              Sign Out
+            </Menu.Item>
           </Sidebar>
+          <Sidebar.Pusher>
+            <Segment basic>
+              
+              <Menu.Item onClick={this.toggleVisibility} >
+                <Icon name="sidebar" />
+              </Menu.Item>
+              <div style={divStyle}>
+                Testing SideBar content
+              </div>
+            </Segment>
+          </Sidebar.Pusher>
         </Sidebar.Pushable>
       </div>
-    )
+    );
   }
 }
 
-export default SidebarLeft
+export default SidebarLeft;
